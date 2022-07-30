caf783d39a32753ea6f2a9d02ee7b3564488814d
Elliot McGonigal <elliot@elliotmcgonigal.com>
2022-07-30 09:31:22 -0700

    add testing for vendor

--- VendorOrder.Tests/ModelTests/VendorTests.cs
+++ VendorOrder.Tests/ModelTests/VendorTests.cs
@@ -6,13 +6,50 @@ using System;
 namespace VendorOrder.Tests
 {
   [TestClass]
-  public class VendorTests
+  public class VendorTests : IDisposable
   {
+    public void Dispose()
+    {
+      Vendor.ClearAll();
+    }
+
     [TestMethod]
     public void MakingTheVendorClass()
     {
-      Vendor testVendor = new Vendor();
+      Vendor testVendor = new Vendor("a", "b");
       Assert.AreEqual(typeof(Vendor), testVendor.GetType());
+    }
+    [TestMethod]
+    public void TestingTheVendorName()
+    {
+      Vendor testVendor = new Vendor("a", "b");
+      Assert.AreEqual("a", testVendor.VendorName);
+    }
+    [TestMethod]
+    public void TestingTheVendorDescription()
+    {
+      Vendor testVendor = new Vendor("a", "b");
+      Assert.AreEqual("b", testVendor.VendorDescription);
+    }
+    [TestMethod]
+    public void TestingTheVendorId()
+    {
+      Vendor testVendor = new Vendor("a", "b");
+      Assert.AreEqual(1, testVendor.Id);
+    }
+    [TestMethod]
+    public void TestingTheVendorGetAll()
+    {
+      Vendor testVendor = new Vendor("a", "b");
+      Vendor testVendor2 = new Vendor("c", "d");
+      List<Vendor> testList = new List<Vendor> {testVendor, testVendor2};
+      CollectionAssert.AreEqual(testList, Vendor.GetAll());
+    }
+    [TestMethod]
+    public void TestingTheVendorFind()
+    {
+      Vendor testVendor = new Vendor("a", "b");
+      Assert.AreEqual(testVendor, Vendor.Find(1));
     }
   }
 }
