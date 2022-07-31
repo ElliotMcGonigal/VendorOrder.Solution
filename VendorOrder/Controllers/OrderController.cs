f79c512a545a221220e836d2b1316bdf9651923b
Elliot McGonigal <elliot@elliotmcgonigal.com>
2022-07-30 23:39:26 -0700

    add ability to display orders

--- VendorOrder/Controllers/OrderController.cs
+++ VendorOrder/Controllers/OrderController.cs
@@ -7,6 +7,21 @@ namespace VendorOrder.Solution.Controllers
 {
     public class OrderController : Controller
     {
-        
+        [HttpGet("/vendor/{vendorId}/order/new")]
+        public ActionResult New(int vendorId)
+        {
+            Vendor findVendor = Vendor.Find(vendorId);
+            return View(findVendor);
+        }
+        [HttpGet("/vendor/{vendorId}/order/{orderId}")]
+        public ActionResult Show(int vendorId, int orderId)
+        {
+            Dictionary<string, object> model = new Dictionary<string, object>();
+            Vendor findVendor = Vendor.Find(vendorId);
+            Order findOrder = findVendor.VendorOrders[orderId];
+            model.Add("vendor", findVendor);
+            model.Add("order", findOrder);
+            return View(model);
+        }
     }
 }
