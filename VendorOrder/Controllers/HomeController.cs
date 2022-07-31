fd5c58b2d7b76ffaa5b967298a86ed5c9db9e1c8
Elliot McGonigal <elliot@elliotmcgonigal.com>
2022-07-31 00:04:02 -0700

    update RESTful conventions

--- VendorOrder/Controllers/HomeController.cs
+++ VendorOrder/Controllers/HomeController.cs
@@ -6,7 +6,7 @@ namespace VendorOrder.Solution.Controllers
 {
     public class HomeController : Controller
     {   
-        [Route("/")]
-        public ActionResult Home() { return View(); }
+        [HttpGet("/")]
+        public ActionResult Index() { return View(); }
     }
 }
